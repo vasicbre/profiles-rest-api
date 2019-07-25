54ee1dcedab97926e836acc8f245217c3401b743
vasicnem <vasicnem@gmail.com>
2019-07-25 15:09:46 +0200

    Added remaining changes from the course

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 from profiles_api import models
 
-#https://www.youtube.com/watch?v=UmljXZIypDc - Django tutorial
 admin.site.register(models.UserProfile)
+admin.site.register(models.ProfileFeedItem)
